717b0101b0a6eefb121e127247b1343b927bf27f
Justin Zupnick <jzupnick@users.noreply.github.com>
2024-02-14 15:59:11 -0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Justin Zupnick on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
